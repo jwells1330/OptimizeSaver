fe8eecf0aa7a9f4c4b1dda06e212fefbc9f5ce2f
Jacob Wells <jwells8@elon.edu>
2016-09-13 10:56:40 -0400

    commit before rename of of all panels to just "Panel"

--- Contact/src/edu/elon/contact/ContactUI.java
+++ Contact/src/edu/elon/contact/ContactUI.java
@@ -6,34 +6,58 @@ import javax.swing.*;
 public class ContactUI {
 
   private JFrame frame;
-  private JPanel panel;
-  private JPanel panel2;
-  private Container container;
-  private JMenu menu;
-  private Dimension maximumSize;
+  private JPanel labelPanel;
+  private JPanel textBoxPanel;
+  private JPanel buttonsPanel;
+  private JButton nextButton;
+  private JButton previousButton;
+  private JMenuBar menuBar;
+  private JMenu file;
+  private JMenu edit;
+  
 
   public ContactUI() {
   }
 
   public void createUI() {
+
+    createFrame();
+    createLabels();
+    createTextBoxes();
+    createButtons();
+    createMenu();
+
+  }
+
+  public void createFrame() {
     frame = new JFrame("Contact Display View");
     frame.setSize(400, 200);
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.getContentPane().setBackground(Color.LIGHT_GRAY);
-    frame.setLayout(new GridLayout(1,2));
+  }
 
-    panel = new JPanel();
-    panel2 = new JPanel();
-    panel.setLayout(new GridLayout(5,1));
-    panel2.setLayout(new GridLayout(5, 1));
-    
-    maximumSize = new Dimension(1, 1);
-    
+  public void createLabels() {
+    labelPanel = new JPanel();
+    labelPanel.setLayout(new GridLayout(5, 1));
+
     JLabel firstName = new JLabel("First Name");
     JLabel middleName = new JLabel("Middle Name");
     JLabel lastName = new JLabel("Last Name");
     JLabel emailLabel = new JLabel("Email");
     JLabel majorLabel = new JLabel("Major");
+
+    labelPanel.add(firstName);
+    labelPanel.add(middleName);
+    labelPanel.add(lastName);
+    labelPanel.add(emailLabel);
+    labelPanel.add(majorLabel);
+    
+    frame.add(labelPanel, BorderLayout.LINE_START);
+  }
+
+  public void createTextBoxes() {
+    textBoxPanel = new JPanel();
+    textBoxPanel.setLayout(new GridLayout(5, 1));
 
     JTextField firstNameBox = new JTextField();
     JTextField middleNameBox = new JTextField();
@@ -41,37 +65,36 @@ public class ContactUI {
     JTextField emailBox = new JTextField();
     JTextField majorBox = new JTextField();
 
-    // Container container = frame.getContentPane();
-    // frame.add(container);
-
-    // firstNameBox.setHorizontalAlignment(JTextField.RIGHT);
-    // middleNameBox.setHorizontalAlignment(JTextField.RIGHT);
-    // lastNameBox.setHorizontalAlignment(JTextField.RIGHT);
-    // emailBox.setHorizontalAlignment(JTextField.RIGHT);
-    // majorBox.setHorizontalAlignment(JTextField.RIGHT);
-
-    firstName.setPreferredSize(maximumSize);
-    panel.add(firstName);
-    panel2.add(firstNameBox);
+    textBoxPanel.add(firstNameBox);
+    textBoxPanel.add(middleNameBox);
+    textBoxPanel.add(lastNameBox);
+    textBoxPanel.add(emailBox);
+    textBoxPanel.add(majorBox);
     
-    
-    panel.add(middleName);
-    panel2.add(middleNameBox);
-    
-    panel.add(lastName);
-    panel2.add(lastNameBox);
-    
-    panel.add(emailLabel);
-    panel2.add(emailBox);
-    
-    panel.add(majorLabel);
-    panel2.add(majorBox);
-    
-    panel.setPreferredSize(maximumSize);
-    frame.add(panel);
-    frame.add(panel2);
+    frame.add(textBoxPanel, BorderLayout.CENTER);
   }
 
+  public void createButtons(){
+    buttonsPanel = new JPanel();
+    nextButton = new JButton("Next");
+    previousButton = new JButton("Previous");
+    
+    buttonsPanel.add(previousButton, BorderLayout.LINE_START);
+    buttonsPanel.add(nextButton, BorderLayout.LINE_END);
+    
+    frame.add(buttonsPanel, BorderLayout.PAGE_END);
+  }
+  
+  public void createMenu(){
+    menuBar = new JMenuBar();
+    file = new JMenu("File");
+    edit = new JMenu("Edit");
+    
+    menuBar.add(file, BorderLayout.WEST);
+    menuBar.add(edit, BorderLayout.CENTER);
+    
+    frame.add(menuBar, BorderLayout.PAGE_START);
+  }
   public void displayUI() {
     frame.setVisible(true);
   }
