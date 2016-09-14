e14a34ec853e8553c5281c576282ab0b3a5bee97
Jacob Wells <jwells8@elon.edu>
2016-09-14 16:38:13 -0400

    began creation of refactored UI

---
+++ src/test/Contact.java
@@ -0,0 +1,51 @@
+package test;
+
+public class Contact {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String email;
+    private String major;
+    
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getMiddleName() {
+        return middleName;
+    }
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getMajor() {
+        return major;
+    }
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    
+    @Override
+    public String toString(){
+        return"" + firstName + "," + middleName + "," + lastName + "," +
+    email + "," + "," + major;
+    }
+    
+    
+    
+    
+}
---
+++ src/test/SQLDatabaseConnector.java
@@ -0,0 +1,14 @@
+package test;
+
+public class SQLDatabaseConnector {
+
+  public static String defaultUser = "jwells1330";
+  public static String defaultPass = "Tw0C0ins";
+  public static String defaultIP = "localhost";
+  public static String defaultDB = "contactBook";
+  public static String defaultTable = "contact";
+  
+  public SQLDatabaseConnector(){
+    
+  }
+}
